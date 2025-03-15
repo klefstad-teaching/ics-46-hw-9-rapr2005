ac1b409bc77915410586d3edf9ea51cc8fb8ac77
rapr2005 <raheali@premjees.com>
2025-03-15 06:59:58 +0000

    .

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -123,9 +123,7 @@ void print_word_ladder(const vector<string>& ladder)
         cout << "Word ladder found: ";
         for (size_t i = 0; i < ladder.size(); ++i) 
         {
-            cout << ladder[i];
-            if (i + 1 < ladder.size()) 
-                cout << " "; 
+            cout << ladder[i] << " ";
         }
         cout << endl; 
     }
